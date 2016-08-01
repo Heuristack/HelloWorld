943ee94ee494605c22bcad93fee1537ec8259654
heuristack <heuristack@gmail.com>
2016-08-02 00:31:48 +0800

    playground: implement a functor class with operator << overloaded

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,13 +1,29 @@
-#include <stdio.h>
-#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+class Functor {
+    char * statement = nullptr;
+public:
+    inline Functor & operator() (char * s = nullptr);
+    friend ostream & operator<< (ostream & s, Functor & f);
+};
+
+Functor & Functor::operator() (char * s)
+{
+    statement = s;
+    return *this;
+}
+
+ostream & operator<< (ostream & s, Functor & f)
+{
+    s << f.statement;
+    return s;
+}
 
 int main()
 {
-    int i = !time(NULL);
-
-    if (!i)
-        puts("No optimization!");
-
-    if (!__builtin_expect(i,0))
-        puts("Weight branch by '__builtin_expect' instruction!");
+    Functor object;
+    char s[] = "Hello,World!";
+    cout << object(s) << endl;
 }
