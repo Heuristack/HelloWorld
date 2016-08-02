c29c9afb152156f2eadda690bc910381d25dc2c3
heuristack <heuristack@gmail.com>
2016-08-02 15:27:21 +0800

    playground: macro __cplusplus and its value

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,27 +1,3 @@
 #include <iostream>
 
-namespace Name
-{
-    namespace Old
-    {
-        const char * f(){ return "old"; }
-    }
-
-    inline
-    namespace New
-    {
-        const char * f(){ return "new"; }
-    }
-}
-
-int main()
-{
-    std::cout << Name::Old::f() << std::endl;
-    std::cout << Name::New::f() << std::endl;
-
-    std::cout << Name::f() << std::endl;
-
-    using namespace Name;
-    std::cout << f() << std::endl;
-
-}
+int main(){ std::cout << __cplusplus << std::endl; }
