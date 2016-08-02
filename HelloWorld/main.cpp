7e6afcaefa30241cea96839376147c9a37357ab4
heuristack <heuristack@gmail.com>
2016-08-02 18:42:35 +0800

    playground: inherited constructors http://www.stroustrup.com/C++11FAQ.html#inheriting

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 
-int main(){ std::cout << __cplusplus << std::endl; }
+struct B { B(char c): m(c){} char m; };
+struct D : B { using B::B; /* implicitly declares D(char) */ };
+
+int main()
+{
+    B b('B');
+    std::cout << b.m << std::endl;
+
+    D d('D');
+    std::cout << d.m << std::endl;
+}
