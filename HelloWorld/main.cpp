88323709ba95fb01f5c4cd01ec27c60ed6b269dc
heuristack <heuristack@gmail.com>
2016-08-04 12:05:39 +0800

    playground: const pointer to char vs pointer to const char http://stackoverflow.com/questions/8091770/const-char-and-char-const-are-they-the-same

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
-int main()
+const char * return_const_pointer(int n = 0)
 {
-    std::vector<int> indices(10);
-    int count = 0;
-    std::generate(indices.begin(), indices.end(), [&count]{ return count++; });
-    std::for_each(indices.begin(), indices.end(), [](int element){ std::cout << element << std::endl; });
+    char const * s = nullptr;
+    switch(n) {
+        case 1:
+            s = "Hello,World!";
+            break;
+        case 2:
+            s = "GoodBye!";
+            break;
+        default:
+            s = " ";
+            break;
+    }
+
+//  char * const t = nullptr;
+//  t = "can't not change!";
+    return s;
 }
+
+int main(){ std::cout << return_const_pointer(1) << return_const_pointer() << return_const_pointer(2) << std::endl; }
