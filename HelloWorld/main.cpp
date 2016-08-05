4e9acb922ad25fd2547574d148bbfcf47f0b9dd6
heuristack <heuristack@gmail.com>
2016-08-06 00:47:15 +0800

    playground: compare the time difference between copying and dereferencing

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -10,13 +10,17 @@ int main()
     string str(p);
 
     chrono::time_point<chrono::high_resolution_clock> s, e;
+    chrono::duration<double> d;
+
     s = chrono::high_resolution_clock::now();
-
     for (int i = 0; i < 10000000; i++) for (const auto & c : str) if (c == 'A');
-    for (int i = 0; i < 10000000; i++) for (auto c : str) if (c == 'A');
-
     e = chrono::high_resolution_clock::now();
-    chrono::duration<double> d = e - s;
+    d = e - s;
+    cout << d.count() << endl;
 
+    s = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 10000000; i++) for (auto c : str) if (c == 'A');
+    e = chrono::high_resolution_clock::now();
+    d = e - s;
     cout << d.count() << endl;
 }
