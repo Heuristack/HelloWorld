93fa938c26a20cdf938951b318c59f255f1c7f77
heuristack <heuristack@gmail.com>
2016-08-12 16:22:04 +0800

    playground: static_assert conditionally issues compile-time error message

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,22 +1,4 @@
-#include <iostream>
-#include <chrono>
-
-using namespace std;
-
 int main()
 {
-
-    const char * p = "Hello,World!";
-    string str(p);
-
-    chrono::time_point<chrono::high_resolution_clock> s, e;
-    s = chrono::high_resolution_clock::now();
-
-    for (int i = 0; i < 10000000; i++) for (const auto & c : str) if (c == 'A');
-    for (int i = 0; i < 10000000; i++) for (auto c : str) if (c == 'A');
-
-    e = chrono::high_resolution_clock::now();
-    chrono::duration<double> d = e - s;
-
-    cout << d.count() << endl;
+    static_assert(true, "Hello,World!");
 }
