2f1c2e25b7c714017cbaf62874ecb531a5aeee75
heuristack <heuristack@gmail.com>
2016-08-15 12:06:26 +0800

    playground: forward list iterator

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,5 +1,5 @@
-#include <unordered_map>
-#include <map>
+#include <forward_list>
+#include <iterator>
 #include <string>
 #include <iostream>
 
@@ -7,14 +7,6 @@ using namespace std;
 
 int main()
 {
-
-    map<string, int> m { {"Dijkstra",1972}, {"Scott",1976}, {"Wilkes",1967}, {"Hamming",1968} };
-    m["Ritchie"] = 1983;
-    for (auto x : m) cout << '{' << x.first << ',' << x.second << '}';
-    cout << endl;
-
-    unordered_map<string, int> um { {"Dijkstra",1972}, {"Scott",1976}, {"Wilkes",1967}, {"Hamming",1968} };
-    um["Ritchie"] = 1983;
-    for (auto x : um) cout << '{' << x.first << ',' << x.second << '}';
-    cout << endl;
+    forward_list<string> list { "Hello", "Forward", "List", "Iterator" };
+    for (forward_list<string>::iterator i = list.begin(); i != list.end(); i++) cout << *i << endl;
 }
