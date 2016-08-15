837ac4fcd785111d815df5537837483ea6d02bca
heuristack <heuristack@gmail.com>
2016-08-15 18:59:16 +0800

    playground: boost asio binding arguments to an asynchronous timer handler

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,18 +1,28 @@
-#include <boost/asio.hpp>
-#include <iostream>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <iostream>
 
-void timer_handler(boost::system::error_code)
+void timer_handler(const boost::system::error_code &, boost::asio::deadline_timer * timer, int * count)
 {
-    std::cout << "time up!" << std::endl;
+    if (*count < 5) {
+        std::cout << *count << std::endl;
+        (*count)++;
+        timer->expires_at(timer->expires_at() + boost::posix_time::seconds(1));
+        timer->async_wait(boost::bind(timer_handler, boost::asio::placeholders::error, timer, count));
+    }
 }
 
 int main()
 {
     boost::asio::io_service service;
     boost::asio::deadline_timer timer(service, boost::posix_time::seconds(5));
+    int count = 0;
     std::cout << "3 2 1 ... " << std::endl;
-    timer.async_wait(timer_handler);
+
+    timer.async_wait(boost::bind(timer_handler, boost::asio::placeholders::error, &timer, &count));
     service.run();
+
+    std::cout << count << std::endl;
     return 0;
 }
