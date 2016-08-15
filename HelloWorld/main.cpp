4fcdf2de63be01da6545c7373e08d2fded53b05d
heuristack <heuristack@gmail.com>
2016-08-15 18:41:55 +0800

    playground: boost asio usage asynchronous deadline timer

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -2,12 +2,17 @@
 #include <iostream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+void timer_handler(boost::system::error_code)
+{
+    std::cout << "time up!" << std::endl;
+}
+
 int main()
 {
     boost::asio::io_service service;
     boost::asio::deadline_timer timer(service, boost::posix_time::seconds(5));
-    timer.wait();
-    std::cout << "time up!" << std::endl;
-
+    std::cout << "3 2 1 ... " << std::endl;
+    timer.async_wait(timer_handler);
+    service.run();
     return 0;
 }
