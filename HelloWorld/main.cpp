4492cc03e6d9be78fdcf88fcd4ae5f945a2161ae
heuristack <heuristack@gmail.com>
2016-08-17 14:36:56 +0800

    playground: transforming data on device

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,11 +8,13 @@ int main()
     boost::compute::context context(device);
     boost::compute::command_queue queue(context, device);
 
-    int host_data[] = {1,3,5,7,9};
-    boost::compute::vector<int> device_vector(5, context);
+    double host_data[] = {1,3,5,7,9};
+    boost::compute::vector<double> device_vector(5, context);
     boost::compute::copy(host_data, host_data + 5, device_vector.begin(), queue);
 
-    std::vector<int> host_vector(5);
+    boost::compute::transform(device_vector.begin(), device_vector.end(), device_vector.begin(), boost::compute::sqrt<double>(), queue);
+
+    std::vector<double> host_vector(5);
     boost::compute::copy(device_vector.begin(), device_vector.end(), host_vector.begin(), queue);
 
     std::cout << device.name() << std::endl;
