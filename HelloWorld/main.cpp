cb63fd72b86efc47f72b8162366819d780d68f04
heuristack <heuristack@gmail.com>
2016-08-17 14:31:22 +0800

    playground: transferring data between host and device

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,11 +1,22 @@
 #include <boost/compute.hpp>
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    boost::compute::device d = boost::compute::system::default_device();
+    boost::compute::device device = boost::compute::system::default_device();
+    boost::compute::context context(device);
+    boost::compute::command_queue queue(context, device);
 
-    std::cout << d.name() << ": " << "Hello,Wrold!" << std::endl;
+    int host_data[] = {1,3,5,7,9};
+    boost::compute::vector<int> device_vector(5, context);
+    boost::compute::copy(host_data, host_data + 5, device_vector.begin(), queue);
+
+    std::vector<int> host_vector(5);
+    boost::compute::copy(device_vector.begin(), device_vector.end(), host_vector.begin(), queue);
+
+    std::cout << device.name() << std::endl;
+    for (const int x : host_vector) std::cout << x << std::endl;
 
     return 0;
 }
