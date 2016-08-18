5396e67698e99bbd545dece4210b3bb9ab5e703f
heuristack <heuristack@gmail.com>
2016-08-18 15:18:44 +0800

    playground: race of 10 threads counting 1 million by 'yield' execution for rescheduling

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include <array>
-#include <iterator>
-#include <algorithm>
-#include <functional>
+#include <thread>
+#include <atomic>
+
+std::atomic<bool> ready(false);
+
+void count1m(int id)
+{
+    while(!ready){
+        std::this_thread::yield();
+    }
+    for (volatile int i = 0; i < 1000000; i++){}
+    std::cout << id;
+}
 
 int main()
 {
-    const int n = 100;
-    std::array<int, n> container;
-    std::generate(container.begin(), container.end(), [=](){ static int x = 0; return x += 1; });
-    std::for_each(container.begin(), container.end(), [=](int e){ std::cout << e << " "; });
+    std::thread threads[10];
+    for (int i = 0; i < 10; i++) threads[i] = std::thread(count1m,i);
+    ready = true;                       // go!
+    for (auto & t : threads) t.join();
     std::cout << std::endl;
-
-    std::cout << container.size() << std::endl;
-    std::cout << std::boolalpha << container.empty() << std::endl;
-
-    // max between the 1st and last element using less functor as the predicate
-    std::cout << std::max(container.front(), container.back(), std::less<int>()) << std::endl;
-    std::cout << std::min(container[0], container.at(container.size())) << std::endl;
-
-    // max in the range of [begin, end) using greater functor as the predicate
-    std::cout << *std::max_element(container.begin(), std::end(container), std::greater<int>{}) << std::endl;
+    return 0;
 }
