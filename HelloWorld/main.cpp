c8cec3b2642d306234476b060283e6c6dd3902be
heuristack <heuristack@gmail.com>
2016-08-18 15:05:33 +0800

    playground: experiments on array + generate & max & max_element & less & greater in std

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,17 +1,24 @@
-#include <tbb/tbb.h>
 #include <iostream>
+#include <array>
+#include <iterator>
+#include <algorithm>
+#include <functional>
 
 int main()
 {
-    class T : public tbb::task {
-        public:
-        tbb::task * execute(){
-            std::cout << "Hello, World!" << std::endl;
-            return nullptr;
-        }
-    };
+    const int n = 100;
+    std::array<int, n> container;
+    std::generate(container.begin(), container.end(), [=](){ static int x = 0; return x += 1; });
+    std::for_each(container.begin(), container.end(), [=](int e){ std::cout << e << " "; });
+    std::cout << std::endl;
 
-    tbb::task_scheduler_init init(tbb::task_scheduler_init::automatic);
-    T & t = *new(tbb::task::allocate_root())T();
-    tbb::task::spawn_root_and_wait(t);
+    std::cout << container.size() << std::endl;
+    std::cout << std::boolalpha << container.empty() << std::endl;
+
+    // max between the 1st and last element using less functor as the predicate
+    std::cout << std::max(container.front(), container.back(), std::less<int>()) << std::endl;
+    std::cout << std::min(container[0], container.at(container.size())) << std::endl;
+
+    // max in the range of [begin, end) using greater functor as the predicate
+    std::cout << *std::max_element(container.begin(), std::end(container), std::greater<int>{}) << std::endl;
 }
