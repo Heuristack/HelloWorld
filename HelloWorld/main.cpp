c5eeba3cbc4435c4e3d4dc445eb4dfdb1274564a
heuristack <heuristack@gmail.com>
2016-08-22 15:16:47 +0800

    playground: observer pattern
    Define a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,14 +1,71 @@
 #include <iostream>
 #include <string>
-#include <memory>
+#include <vector>
+
+namespace design_pattern {
+
+class subject {
+    std::vector<class observer *> observers_;
+public:
+    void attach(class observer * o) {
+        observers_.push_back(o);
+    }
+    void notify();
+};
+
+class observer {
+    class subject * subject_ = nullptr;
+public:
+    observer() = default;
+    observer(class subject *s): subject_(s) { subject_->attach(this); }
+    class subject * get_subject() { return subject_; }
+    void set_subject(class subject *s) { subject_ = s; subject_->attach(this); }
+    virtual void update() = 0;
+};
+
+void subject::notify() {
+    for (class observer * o : observers_)
+        o->update();
+}
+
+} //namespace
 
 int main()
 {
-    struct T {
-        std::string s_;
-        int n_;
-        T(const char * s, int n): s_(s), n_(n) {}
+    class concrete_subject : public design_pattern::subject {
+        std::string subject_state_ = "s";
+    public:
+        std::string const & get_state() const { return subject_state_; }
+        void set_state(std::string const & state) {
+            subject_state_ = state;
+            notify();
+        }
     };
 
-    std::cout << std::make_unique<T>("Hello,World!", 8848)->s_ << std::endl;
+    class concrete_observer : public design_pattern::observer {
+        std::string subject_state_ = "o";
+    public:
+        using design_pattern::observer::observer;
+        void update() override {
+            //
+            // Notified something dependent is changed, recaculate internal state
+            //
+            subject_state_ = static_cast<concrete_subject*>(get_subject())->get_state();
+        }
+        std::string const & get_state() const { return subject_state_; }
+    };
+
+    concrete_subject s;
+    std::vector<concrete_observer> O;
+    O.push_back(*(new concrete_observer(&s)));
+    O.push_back(*(new concrete_observer(&s)));
+
+    std::vector<concrete_observer*> R;
+    R.push_back(new concrete_observer(&s));
+    R.push_back(new concrete_observer(&s));
+
+    s.set_state("notice ... ");
+    std::cout << "S: " << s.get_state() << std::endl;
+    for (concrete_observer & o : O) std::cout << "O: " <<  o.get_state() << std::endl;
+    for (concrete_observer * r : R) std::cout << "R: " << r->get_state() << std::endl;
 }
