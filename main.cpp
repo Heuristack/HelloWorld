e9df8bc8daed905956b2f929120f51d95fa56238
heuristack <heuristack@gmail.com>
2016-08-22 22:42:09 +0800

    elements of programming interviews - problem 5.2: compute quotient of natural numbers
    skill: Euclidean Division (Algorithm) reflects the nature of numbers.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-#include <bitset>
 
-using std::cout;
-using std::endl;
-using std::bitset;
-
-bool parity(unsigned long n)
+unsigned divide(unsigned numerator, unsigned denominator)
 {
-    bitset<CHAR_BIT * sizeof(unsigned long)> s(n);
-    if (s.count() % 2)
-        return true;
-    return false;
+    unsigned quotient = 0;
+    while (numerator >= denominator) {
+        numerator -= denominator;
+        quotient++;
+    }
+    return quotient;
 }
 
 int main()
 {
-    unsigned long n = 0B0000000000000000000111001001001010010111001000000010111101010100UL;
-    bitset<CHAR_BIT * sizeof(unsigned long)> s(n);
-    cout << "parity(0B" << s << "UL = " << n << ") = " << parity(n) << endl;
-    return 1;
+    std::cout << divide(8,1) << std::endl;
 }
