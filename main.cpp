0d5098122fff9d386e3c8f6b52729144550b054d
Lance LI <heuristack@gmail.com>
2016-08-23 15:46:11 +0800

    playground: numerics library NaN and Infinity functions
    Note: difference between inclusion of <cmath> and <math.h> is on the namespace (i.e., std::, ::).

--- main.cpp
+++ main.cpp
@@ -1,67 +1,15 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-
-#include <ctype.h>
-
-using std::string;
-using std::reverse;
-
-inline int tonum(char digit)
-{
-    int num = 0;
-
-    if (isdigit(digit))
-        num = digit - '0';
-    else
-        num = 10 + digit - 'A';
-
-    return num;
-}
-
-inline char todigit(int num)
-{
-    char digit;
-
-    if (num >= 10)
-        digit = 'A' + num - 10;
-    else
-        digit = '0' + num;
-
-    return digit;
-}
-
-string convert_base(string s, int b1, int b2)
-{
-    string z;
-
-    bool negative = s.front() == '-';
-
-    int n = 0;
-    for (string::iterator i = s.begin() + ((negative) ? 1 : 0); i < s.end(); i++) {
-        n *= b1;
-        n += tonum(*i);
-    }
-
-    int r = 0;
-    while (n) {
-        r = n % b2;
-        z.push_back(todigit(r));
-        n /= b2;
-    }
-
-    if (z.empty())
-        z.push_back('0');
-
-    if (negative)
-        z.push_back('-');
-
-    reverse(z.begin(), z.end());
-
-    return z;
-}
+#include <cmath>
 
 int main()
 {
-    std::cout << convert_base("56789", 10, 16) << std::endl;
+    double not_a_number = NAN;
+    double double_precision_floating_point_number = INFINITY;
+
+    std::cout << std::isnan(not_a_number) << std::endl;
+    std::cout << std::isinf(double_precision_floating_point_number) << std::endl;
+    std::cout << std::isfinite(double_precision_floating_point_number) << std::endl;
+
+    std::cout << std::numeric_limits<int>::infinity() << std::endl;
+    std::cout << std::numeric_limits<double>::infinity() << std::endl;
 }
