4de6be30e6eb61f4774cb8155449d17ce923ac61
Lance LI <heuristack@gmail.com>
2016-08-24 14:31:01 +0800

    playground: member function can indirectly (implicitly) call its destructor through smart pointer (unique_prt)
    note: even the destructor is called in a member function, the function can still finish its execution.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include <string>
 #include <memory>
 
-class T {
-    int m = 0;
-public:
-    T(int n):m(n){ std::cout << "C: " << m << std::endl; }
-    ~T(){ std::cout << "D: " << m << std::endl; }
-};
-
 int main()
 {
+    class T {
+        int m = 0;
+    public:
+        T(int n):m(n){ std::cout << "C: " << m << std::endl; }
+        ~T(){ std::cout << "D: " << m << std::endl; m = 0; }
+        void f(std::unique_ptr<T>& p){ p = nullptr; std::cout << "F: " << m << std::endl; }
+    };
+
     auto p = std::make_unique<T>(1);
     std::cout << "points to 1" << std::endl;
 
@@ -19,4 +20,6 @@ int main()
 
     p = std::make_unique<T>(2);
     std::cout << "points to 2" << std::endl;
+
+    p->f(p);
 }
