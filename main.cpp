47d86acd33c517ad194dcae2e66fed769d666b7d
Lance LI <heuristack@gmail.com>
2016-08-25 11:56:36 +0800

    playground: delegating constructor allows to define one constructor in terms of another (since C++11)

--- main.cpp
+++ main.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <forward_list>
-#include <deque>
-#include <list>
-#include <algorithm>
-#include <numeric>
+#include <string>
 
 using namespace std;
 
+class X {
+    int m = -1;
+    ostream & puts(ostream & s){ return s << m; }
+public:
+    X(int x){ if (x > 80 && x < 90) m = x; }
+    X(): X(85){}
+    X(string s): X(stoi(s)){}
+    friend ostream & operator<<(ostream & s, X & x){ return x.puts(s); }
+};
+
 int main()
 {
-    vector<int> vector(10,1);
-    forward_list<int> forward_list(10);
-    deque<int> deque(10);
-    list<int> list(10);
-    partial_sum(vector.begin(), vector.end(), vector.begin());
-
-    cout << *rotate_copy(vector.begin(), vector.begin()+1, vector.end(), forward_list.before_begin()) << endl;
-    cout << *rotate_copy(vector.begin(), vector.begin()+1, vector.end(), list.begin()) << endl;
-    cout << *rotate_copy(vector.begin(), vector.begin()+1, vector.end(), deque.begin()) << endl;
-
-    for (auto & e : vector) cout << e << " "; cout << endl;
-    for (auto & e : forward_list) cout << e << " "; cout << endl;
-    for (auto & e : list) cout << e << " "; cout << endl;
-    for (auto & e : deque) cout << e << " "; cout << endl;
+    X x("88");
+    cout << x << endl;
 }
