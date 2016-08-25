8c5387fab35ec3318423d06267351cee6dd94c28
Lance LI <heuristack@gmail.com>
2016-08-25 12:29:14 +0800

    playground: destruct oneself by invoking destructor implicitly through 'delete this'
    think: what happened after 'delete this'?

--- main.cpp
+++ main.cpp
@@ -7,14 +7,22 @@ class X {
     int m = -1;
     ostream & puts(ostream & s){ return s << m; }
 public:
-    X(int x){ if (x > 80 && x < 90) m = x; }
-    X(): X(85){}
+    X(int x){ m = x; cout << "C: " << m << endl; }
+    X(): X(100){}
     X(string s): X(stoi(s)){}
+    ~X(){ cout << "D: " << m << endl; }
+
+    int delete_self(X * p){ delete p; return m; }
+    int delete_self(){ delete this; /* Herein, what can I do after I deleted myself? */ return m; }
+
+    friend int delete_friend(X * p);
     friend ostream & operator<<(ostream & s, X & x){ return x.puts(s); }
 };
 
+int delete_friend(X * p){ delete p; return p->m; }
+
 int main()
 {
-    X x("88");
-    cout << x << endl;
+    X * p = new X(99);
+    cout << p->delete_self() << endl;
 }
