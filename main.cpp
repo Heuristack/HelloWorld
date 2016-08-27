f86681c9973a499d44a5ffdf373744fe1fd9e279
heuristack <heuristack@gmail.com>
2016-08-27 15:32:00 +0800

    playground: design pattern - adaptor using multiple inheritance

--- main.cpp
+++ main.cpp
@@ -1,69 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 
-namespace design_pattern
-{
-    using std::cout; using std::endl;
+using namespace std;
 
-    class Implementation {
-    public:
-        Implementation(){ cout << "I"; }
-        virtual~Implementation(){ cout << "I"; }
-        virtual void implement() = 0;
-    };
+class Target {
+public:
+    virtual void operate(){ cout << "T"; }
+};
 
-    class ConcreteImplementation : public Implementation {
-    public:
-       ~ConcreteImplementation(){ cout << "C"; }
-        ConcreteImplementation(){ cout << "C"; }
-        virtual void implement() override { cout << "&"; }
-    };
+class Source {
+public:
+    virtual void operate(){ cout << "S"; }
+};
 
-    class DefiniteImplementation : public Implementation {
-    public:
-       ~DefiniteImplementation(){ cout << "D"; }
-        DefiniteImplementation(){ cout << "D"; }
-        virtual void implement() override { cout << "*"; }
-    };
-
-    class Abstraction {
-        Implementation * implementation;    // The conceptal 'bridge', isn't it?
-    public:
-       ~Abstraction(){ cout << "A"; }
-        Abstraction(Implementation * imp): implementation(imp){ cout << "A"; }
-        virtual void operate(){ implementation->implement(); }
-    };
-
-    class DevelopedAbstraction : public Abstraction {
-    public:
-        using Abstraction::Abstraction;
-        virtual void operate() override { cout << " Developed("; Abstraction::operate(); cout << ") "; }
-    };
-
-    class OptimizedAbstraction : public Abstraction {
-    public:
-        using Abstraction::Abstraction;
-        virtual void operate() override { cout << " Optimized("; Abstraction::operate(); cout << ") "; }
-    };
-}
-
-using namespace design_pattern;
+class Adaptor : public Target, private Source {
+public:
+    virtual void operate() override { Source::operate(); }
+};
 
 int main()
 {
-    Implementation * concrete = new ConcreteImplementation();
-    Implementation * definite = new DefiniteImplementation();
-
-    Abstraction * optimized = new OptimizedAbstraction(concrete);
-    Abstraction * developed = new DevelopedAbstraction(definite);
-
-    optimized->operate();
-    developed->operate();
-
-    delete developed;
-    delete optimized;
-    delete definite;
-    delete concrete;
-
-    std::atexit([](){ cout << endl; });
+    Target * t = new Adaptor();
+    t->operate();
+    delete t;
+    atexit([](){ cout << endl; });
 }
