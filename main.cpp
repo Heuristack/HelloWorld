661817f419960932dd5d297b1772885be92eca37
heuristack <heuristack@gmail.com>
2016-08-29 15:26:04 +0800

    playground: numeric limits for built-in numeric types [digits, min, max, epsilon, ...]

--- main.cpp
+++ main.cpp
@@ -1,48 +1,24 @@
 #include <iostream>
 #include <utility>
-#include <string>
+#include <typeinfo>
+#include <typeindex>
 
 using namespace std;
 
+template<typename T> void print_nemuric_limits()
+{
+    cout << "Nemuric Limits for Type: " << typeid(T).name() << endl;
+    using limits = numeric_limits<T>;
+    cout << "digits: " << limits::digits << endl;
+    cout << "max: " << limits::max() << endl;
+    cout << "min: " << limits::min() << endl;
+    cout << "is exact: " << limits::is_exact << endl;
+    cout << "epsilon: " << limits::epsilon() << endl;
+    cout << endl;
+}
+
 int main()
 {
-    // min and max
-    cout << min({2,4,6,8,3}) << endl;
-    cout << max(nano::den, mega::num) << endl;
-    cout << minmax({10,2,4,8,1}).first << endl;
-
-    // ratio
-    typedef typename ratio<1,30>::type onethird_t;
-    cout << "1/30 := [numerator = " << onethird_t::num << "; " << "denominator = " << onethird_t::den << "]" << endl;
-    cout << "nano := [numerator = " << nano::num << "; " << "denominator = " << nano::den << "]" << endl;
-    cout << exa::num << endl;
-
-    // comparison operators deduction
-    class X {
-        string state = "0";
-    public:
-        X(const string & s): state(s){}
-        X(): X(""){}
-        bool operator==(const X & that_reference) const { return state[0] == that_reference.state[0]; }
-        bool operator <(const X & that_reference) const { return state[0]  < that_reference.state[0]; }
-        ostream & puts(ostream & s) const { return s << "X = [" << state << "]"; }
-    };
-
-    using namespace std::rel_ops;
-
-    X this_x("1");
-    X that_x("2");
-    if (this_x != that_x) cout << "!=" << endl;
-    if (this_x <= that_x) cout << "<=" << endl;
-
-    // swap
-    swap(this_x, that_x);
-    if (this_x >  that_x) cout << "> " << endl;
-    if (this_x >= that_x) cout << ">=" << endl;
-
-    this_x.puts(cout) << endl;
-    that_x.puts(cout) << endl;
-
-    min({this_x, that_x}).puts(cout) << endl;
-    max({this_x, that_x}).puts(cout) << endl;
+    print_nemuric_limits<unsigned long long>();
+    print_nemuric_limits<double>();
 }
