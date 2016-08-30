24df287ec4de772b37a322ffce18321d4f3cfa57
heuristack <heuristack@gmail.com>
2016-08-30 15:15:45 +0800

    playground: last legal position to 'rotate' and erase operation of standard containers

--- main.cpp
+++ main.cpp
@@ -1,32 +1,24 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 
+class T {
+    int state = -1;
+public:
+    T(int s): state(s){}
+    T(): T(0){}
+    int get () const { return state; }
+};
 using namespace std;
-
-class Prototype {
-public:
-    virtual auto clone() -> Prototype * = 0;
-};
-
-class ConcretePrototype : public Prototype {
-    string state;
-public:
-   ~ConcretePrototype() = default;
-    ConcretePrototype(const string & s): state(s){}
-    ConcretePrototype(): ConcretePrototype("Initialized by default!"){}
-    ConcretePrototype(const ConcretePrototype & that_reference){ state = that_reference.state; }
-    virtual auto clone() -> ConcretePrototype * override { return new ConcretePrototype(*this); }
-
-    friend ostream & operator<<(ostream & s, ConcretePrototype & o){ return s << "[" << o.state << "]"; }
-};
-
 int main()
 {
-    ConcretePrototype prototype("prototype");
+    std::vector<T> v = {T(), T{2}};
 
-    ConcretePrototype * cloned = prototype.clone();
-    cout << *cloned << endl;
-    delete cloned;
+    auto last_legal_position_to_retate = v.begin() + v.size();
+    rotate(v.begin(), last_legal_position_to_retate, v.end());
+    cout << v.front().get() << endl;
 
-    cout << prototype << endl;
+    v.erase(v.begin());
+    cout << v.front().get() << endl;
+    cout << v[0].get() << endl;
 }
