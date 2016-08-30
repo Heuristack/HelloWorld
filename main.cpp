479850edd9a6c18e476b7a1965a34a4a56209ed8
heuristack <heuristack@gmail.com>
2016-08-31 00:40:03 +0800

    playground: design pattern - visitor ['visitor' implementation class defines a new operation without changing the classes of the elements on which it operates]

--- main.cpp
+++ main.cpp
@@ -1,34 +1,60 @@
 #include <iostream>
 #include <string>
-#include <memory>
+#include <typeinfo>
 
 using namespace std;
 
-class C {
-    int internal_state = 10;
+class Element {
+public:
+    virtual ~Element(){}
+    virtual void accept(class Visitor & visitor) = 0;
+    static string classname(){ return typeid(Element).name(); }
 };
 
-class T
-{
-    int internal_state = 20;
-    string state;
+class ConcreteElement : public Element {
 public:
-   ~T(){ cout << "D" << endl; }
-    T(): T("Initialized by default!"){}
-    T(const string & s): state(s){ cout << "C" << endl; }
+    void accept(Visitor & visitor) override;
+    string classname(){ return typeid(*this).name(); }
+};
 
-    const string & get(){ return state; }
-    int get_value(){ return internal_state; }
-    unique_ptr<T> clone(){ auto p = make_unique<T>(state); p->internal_state = internal_state; return p; }
+class DefiniteElement : public Element {
+public:
+    void accept(Visitor & visitor) override;
+    string classname(){ return typeid(*this).name(); }
+};
+
+class Visitor {
+public:
+    virtual void visit(ConcreteElement * element) = 0;
+    virtual void visit(DefiniteElement * element) = 0;
+};
+
+void ConcreteElement::accept(Visitor & visitor){ visitor.visit(this); }
+void DefiniteElement::accept(Visitor & visitor){ visitor.visit(this); }
+
+class ForewardVisitor : public Visitor {
+public:
+    void visit(ConcreteElement * element) override { cout << "Foreward -> " << element->classname() << endl; }
+    void visit(DefiniteElement * element) override { cout << "Foreward -> " << element->classname() << endl; }
+};
+
+class BackwardVisitor : public Visitor {
+public:
+    void visit(ConcreteElement * element) override { cout << "Backward -> " << element->classname() << endl; }
+    void visit(DefiniteElement * element) override { cout << "Backward -> " << element->classname() << endl; }
 };
 
 int main()
 {
-    unique_ptr<T> originalptr = make_unique<T>("original");
-    cout << originalptr->get() << endl;
+    ForewardVisitor fv;
+    BackwardVisitor bv;
 
-    unique_ptr<T> clonedptr = originalptr.get()->clone();
-    cout << clonedptr->get() << endl;
-    cout << clonedptr->get_value() << endl;
+    ConcreteElement concrete;
+    DefiniteElement definite;
 
+    concrete.accept(fv);
+    concrete.accept(bv);
+
+    definite.accept(fv);
+    definite.accept(bv);
 }
