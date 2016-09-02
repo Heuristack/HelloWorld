ef7cf4d852b24cf3397887ccc80503f12a08a3d4
heuristack <heuristack@gmail.com>
2016-09-02 16:30:31 +0800

    playground: call to a function could be ambiguous introduced by function overloading and default argument mechanism

--- main.cpp
+++ main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <string>
-#include <algorithm>
+
 using namespace std;
+
+void noordefault(){ cout << "0" << endl; }
+void noordefault(int m = 0){ cout << "1" << endl; }
 
 int main()
 {
-    vector<string> dynamic_array = {"A", "B", "C", "D"};
-    set<string> binary_search_tree(dynamic_array.begin(), dynamic_array.end());
-
-    rotate(dynamic_array.begin(), dynamic_array.begin()+1, dynamic_array.end());
-    for (string e : dynamic_array) cout << e << " ";
-    cout << endl;
-
-    rotate(dynamic_array.rbegin(), dynamic_array.rbegin()+1, dynamic_array.rend());
-    for (string e : dynamic_array) cout << e << " ";
-    cout << endl;
-
-    vector<string>::const_iterator arraypos = find(dynamic_array.begin(), dynamic_array.end(), "C");
-    set<string>::const_iterator treepos = binary_search_tree.find("C");
-
-    dynamic_array.erase(arraypos);
-    binary_search_tree.erase(treepos);
-
-    for (string e : dynamic_array) cout << e << " "; cout << endl;
-    for (string e : binary_search_tree) cout << e << " "; cout << endl;
+//  noordefault();  // this call is ambiguous
+    noordefault(10);
 }
