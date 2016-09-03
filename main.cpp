3fca97c03aa149d77e452c76e32cd35d646f3522
heuristack <heuristack@gmail.com>
2016-09-04 00:08:57 +0800

    playground: design pattern - interpreter: abstract expression and its interpret method [implements by nonterminal and terminal expression]

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-#include <string>
+#include <list>
+#include <memory>
 using namespace std;
+
+class Context {};
+
+class AbstractExpression {
+public:
+    virtual ~AbstractExpression(){}
+    virtual void interpret(Context &) = 0;
+};
+
+class TerminalExpression : public AbstractExpression
+{
+public:
+    void interpret(Context & context) override { cout << "T"; }
+};
+class NonterminalExpression : public AbstractExpression {
+public:
+    void interpret(Context & context) override { cout << "N"; }
+};
 
 int main()
 {
-    char inputbuf[0xFF];
-    while (cin.getline(inputbuf, 0xFF, '\n')) cout << string(inputbuf) << endl;
+    Context context;
+
+    list<unique_ptr<AbstractExpression>> abstract_syntax_tree;
+
+    abstract_syntax_tree.push_back(make_unique<TerminalExpression>());
+    abstract_syntax_tree.push_back(make_unique<NonterminalExpression>());
+    abstract_syntax_tree.push_back(make_unique<TerminalExpression>());
+    abstract_syntax_tree.push_back(make_unique<TerminalExpression>());
+
+    for (const unique_ptr<AbstractExpression> & expr : abstract_syntax_tree)
+        expr->interpret(context);
+
+    cout << endl;
 }
