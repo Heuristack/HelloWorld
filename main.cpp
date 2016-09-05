f39a343a6883adf67ff63b8bb3d058a66e0d3711
heuristack <heuristack@gmail.com>
2016-09-05 14:13:24 +0800

    leetcode question 392 - is subsequence: time complexity = O(n), constant space [extreme situation: empty string]

--- main.cpp
+++ main.cpp
@@ -2,58 +2,21 @@
 #include <string>
 using namespace std;
 
-class Colleague;
-class Mediator {
-public:
-    virtual ~Mediator(){}
-    virtual void send(string message, Colleague * colleague) = 0;
-};
-
-class ConcreteMediator : public Mediator {
-    Colleague * thisone = nullptr;
-    Colleague * thatone = nullptr;
-public:
-    void send(string message, Colleague * colleague);
-    void registerthis(Colleague * colleague){ thisone = colleague; }
-    void registerthat(Colleague * colleague){ thatone = colleague; }
-};
-
-class Colleague {
-protected:
-    Mediator & mediator;
-public:
-    Colleague(Mediator & m): mediator(m){}
-    void send(string message){ mediator.send(message, this); }
-    virtual void notify(string message) = 0;
-};
-
-void ConcreteMediator::send(string message, Colleague * colleague) {
-    if (colleague == thisone) thatone->notify(message);
-    if (colleague == thatone) thisone->notify(message);
+bool is_subsequence(string s, string t)
+{
+    if (s.empty()) return true;
+    int i = 0;
+    for (int k = 0; k < t.size(); k++) {
+        if (s[i] == t[k]) i++;
+        if (i == s.size() - 1) break;
+    }
+    if (i == s.size() - 1) return true;
+    else return false;
 }
-
-class ConcreteColleague : public Colleague {
-public:
-    using Colleague::Colleague;
-    void notify(string message){ cout << "ConcreteColleague: " << message << endl; }
-};
-
-class DefiniteColleague : public Colleague {
-public:
-    using Colleague::Colleague;
-    void notify(string message){ cout << "DefiniteColleague: " << message << endl; }
-};
 
 int main()
 {
-    ConcreteMediator mediator;
-    ConcreteColleague concrete(mediator);
-    DefiniteColleague definite(mediator);
-
-    mediator.registerthis(&concrete);
-    mediator.registerthat(&definite);
-
-    concrete.send("Hi! How are you?");
-    definite.send("I am fine! Thank you! And you?");
-    concrete.send("I am fine too!");
+    cout << is_subsequence("", "ahbgdc") << endl;
+    cout << is_subsequence("abc", "ahbgdc") << endl;
+    cout << is_subsequence("axc", "ahbgdc") << endl;
 }
