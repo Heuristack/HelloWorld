a6a9fb79579b14b47d83bc9c819c5d10594726f8
heuristack <heuristack@gmail.com>
2016-09-05 22:55:44 +0800

    leetcode question 2 - add two numbers: rumtime 25ms beating 100% of cppsubmmissions

--- main.cpp
+++ main.cpp
@@ -1,25 +1,80 @@
+#include <cassert>
 #include <iostream>
-#include <vector>
-#include <unordered_map>
+#include <list>
 using namespace std;
 
-vector<int> two_sum(vector<int> nums, int target)
+struct ListNode {
+    int val;
+    ListNode * next = nullptr;
+    ListNode(int x):val(x), next(nullptr){}
+};
+
+void print_list(ListNode * l)
 {
-    vector<int> two;
-    unordered_map<int, int> hashtable;
-    for (int i = 0; i < nums.size(); i++) {
-        auto r = hashtable.find(nums[i]);
-        if (r != hashtable.end()) {
-            two.push_back(r->second);
-            two.push_back(i);
-        }
-        int tomatch = target - nums[i];
-        hashtable.insert(make_pair(tomatch, i));
+    cout << "[";
+    for (ListNode * p = l; p != nullptr; p = p->next) {
+        cout << p->val;
+        if (p->next) cout << ",";
     }
-    return two;
+    cout << "]" << endl;
+}
+
+ListNode * create_list(list<int> integer_list)
+{
+    ListNode * head = nullptr, * tail = nullptr;
+    
+    for (list<int>::iterator i = integer_list.begin(); i != integer_list.end(); ++i) {
+        int v = *i;
+        ListNode * current = new ListNode(v);
+        if (head == nullptr) { tail = current; head = tail; }
+        else {
+            assert(tail->next == nullptr);
+            tail->next = current;
+            assert(tail->next != nullptr);
+            tail = tail->next;
+        }
+    }
+    print_list(head);
+    return head;
+}
+
+ListNode * add_two_numbers(ListNode * l1, ListNode * l2)
+{
+    ListNode * head = nullptr, * tail = nullptr;
+    int last_quotient = 0;
+    for (ListNode * p1 = l1, * p2 = l2; p1 != nullptr || p2 != nullptr; ) {
+        int sum = last_quotient;
+        if (p1 != nullptr) sum += p1->val;
+        if (p2 != nullptr) sum += p2->val;
+        last_quotient = sum / 10;
+        sum = sum % 10;
+        
+        ListNode * current = new ListNode(sum);
+        if (head == nullptr) { tail = current; head = tail; }
+        else {
+            assert(tail->next == nullptr);
+            tail->next = current;
+            assert(tail->next != nullptr);
+            tail = tail->next;
+        }
+        
+        if (p1 != nullptr) p1 = p1->next;
+        if (p2 != nullptr) p2 = p2->next;
+    }
+    if (last_quotient > 0) {
+        ListNode * last = new ListNode(last_quotient);
+        assert(tail->next == nullptr);
+        tail->next = last;
+        assert(tail->next != nullptr);
+        tail = tail->next;
+    }
+    return head;
 }
 
 int main()
 {
-    for (auto x : two_sum({2, 7, 11, 15}, 9)) cout << x << endl;
+    ListNode * l1 = create_list({2,3,5,9});
+    ListNode * l2 = create_list({1,7,6});
+    
+    print_list(add_two_numbers(l1, l2));
 }
