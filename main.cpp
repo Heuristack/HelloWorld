94a773cfc0796ef866886bafc4dd8288acc7fbf9
heuristack <heuristack@gmail.com>
2016-09-06 16:46:59 +0800

    playground: balanced tree structured containters - set and multiset [lower_bound, upper_bound, equal_range,etc]

--- main.cpp
+++ main.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
+#include <set>
+#include <iterator>
 using namespace std;
 
-int lengthOfLongestSubstring(string s)
+template<typename container_type, typename iterator_type = typename container_type::iterator>
+void print(const container_type & tree)
 {
-    string::size_type longest = 0;
-    unordered_map<char, string::size_type> hashtable;
-    for (string::size_type p = 0, q = 0; q < s.size(); q++) {
-        string::size_type thislength = q - p + 1;
-        if (thislength >= longest) { longest = thislength; }
-        hashtable[s[q]] = q;
-        if (q + 1 < s.size()) {
-            auto i = hashtable.find(s[q+1]);
-            if (i != hashtable.end() && i->second >= p ){
-                p = i->second + 1;
-                hashtable[s[p]] = p;
-            }
-        }
-    }
-    return longest;
+    cout << "{";
+    for (iterator_type i = tree.begin(); i != tree.end(); i++) { cout << *i; if (next(i) != tree.end()) cout << ","; }
+    cout << "}";
+    cout << endl;
+}
+
+template<typename container_type, typename iterator_type = typename container_type::iterator, typename value_type = typename container_type::value_type>
+void bounds(const container_type & tree, value_type value)
+{
+    cout << "lower bound = " << *tree.lower_bound(value) << endl;
+    cout << "upper bound = " << *tree.upper_bound(value) << endl;
+    iterator_type p, q;
+    tie(p, q) = tree.equal_range(value);
+    cout << "equal range = " << *p << ":" << *q << endl;
+
+    cout << "elements in the equal range := [" << *p << "," << *q << ") = {";
+    for (iterator_type i = tree.lower_bound(value); i != tree.upper_bound(value); i++) { cout << *i; if (next(i) != tree.upper_bound(value)) cout << ","; }
+    cout << "}";
+    cout << endl;
 }
 
 int main()
 {
-    cout << lengthOfLongestSubstring("aaab") << endl;
+    set<int> tree = {1,2,4,5,6};
+    print(tree);
+
+    bounds(tree, 3);
+    bounds(tree, 5);
+
+    multiset<int> multitree = {1,2,2,2,4,4,5,5,5,5,6};
+    print(multitree);
+
+    bounds(multitree, 3);
+    bounds(multitree, 5);
 }
