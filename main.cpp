1048f2d2d7c6dc4a944cc16fb9eb210c5bcc19e7
heuristack <heuristack@gmail.com>
2016-09-26 15:54:43 +0800

    playground: unique_ptr<T> initialization, release and empty status checking by operator bool(), == nullptr, and get()

--- main.cpp
+++ main.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
 #include <string>
-#include <system_error>
-#include <cerrno>
-#include <unistd.h>
-#include <boost/filesystem.hpp>
-#include <readline/history.h>
-#include <readline/readline.h>
-
+#include <memory>
 using namespace std;
 
 int main()
 {
-    cout << "Current Working Directory: " << boost::filesystem::current_path() << endl;
-    int return_code = 0;
+    // can't be initialized by assignment but only by constructor
+    unique_ptr<string> up(new string("Hello,World!"));
 
-    HISTORY_STATE * state = history_get_history_state();
-    cout << "history list length = " << state->length << endl;
+    // release the ownership
+    auto rp = up.release();
+    cout << *rp << endl;
 
-    return_code = read_history(NULL);
-    cout << "read history return code = " << return_code << " : " << strerror(return_code) << endl;
-
-    return_code = add_history("Hello,World!");
-    cout << "add history entry return code = " << return_code << " : " << strerror(return_code) << endl;
-
-    HIST_ENTRY * entry = current_history();
-    cout << "current history line: " << entry->line << "; data: " << entry->data << endl;
-    cout << "current history offset: " << where_history() << endl;
-    cout << "history total bytes: " << history_total_bytes() << endl;
+    // own an object or not? operator bool()
+    if (up == nullptr || !up || !up.get()) cout << "Empty unique pointer!" << endl;
 }
