3296e4225cfce2114b72dd81f399cf338c0b7f7b
heuristack <heuristack@gmail.com>
2016-09-26 16:04:26 +0800

    playground: how to validate objects in boolean context - idiomatic = if(object){}
    http://www.artima.com/cppsource/safebool.html

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-#include <string>
-#include <memory>
 using namespace std;
+
+class Testable {
+    bool status = true;
+public:
+   ~Testable() = default;
+    Testable() = default;
+    Testable(bool s): status(s){}
+
+    operator bool(){ return status; }
+};
 
 int main()
 {
-    // can't be initialized by assignment but only by constructor
-    unique_ptr<string> up(new string("Hello,World!"));
-
-    // release the ownership
-    auto rp = up.release();
-    cout << *rp << endl;
-
-    // own an object or not? operator bool()
-    if (up == nullptr || !up || !up.get()) cout << "Empty unique pointer!" << endl;
+    Testable condition(false);
+    if (!condition) cout << "!" << endl;
 }
