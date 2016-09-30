5d9f010a7a19d332db4460f9f3f32076423a0fcb
heuristack <heuristack@gmail.com>
2016-09-30 11:42:05 +0800

    playground: parse an input stream consisting of items seperated by delimiter into string tokens

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
+#include <sstream>
 #include <string>
-#include <exception>
 
 using std::cout;
 using std::endl;
 
 int main()
 {
-    try { cout << std::stoi("NaN") << endl; }
-    catch (std::logic_error & e) { cout << "logic error: " << e.what() << endl; }
-    catch (std::runtime_error & e) { cout << "runtime error: " << e.what() << endl; }
-    catch (std::exception & e) { cout << "exception: " << e.what() << endl; }
+    std::stringstream stream {"a,b,c"};
+    for (std::string s; std::getline(stream, s, ','); cout << s << endl);
 }
