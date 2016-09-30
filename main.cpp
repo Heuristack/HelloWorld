cc10e074fb0d554e8a3a7b65ed6daa0d90c51843
heuristack <heuristack@gmail.com>
2016-09-30 11:27:52 +0800

    playground: exception thrown on std::stoi(NaN) -> logic error : exception

--- main.cpp
+++ main.cpp
@@ -1,21 +1,14 @@
-#include <system_error>
-#include <ios>
-#include <future>
 #include <iostream>
+#include <string>
+#include <exception>
 
 using std::cout;
 using std::endl;
 
 int main()
 {
-    cout << std::is_error_code_enum<std::errc>::value << endl;
-
-//  std::error_code ec(EAFNOSUPPORT, std::generic_category());
-
-    std::error_code ec = std::make_error_code(std::errc::address_family_not_supported);
-//  std::error_code ec = std::make_error_code(std::io_errc::stream);
-//  std::error_code ec = std::make_error_code(std::future_errc::no_state);
-
-    cout << "[" << ec.value() << ":" << ec.category().name() << ":\"" << ec.category().message(ec.value()) << "\":\"" << ec.message() << "\"]" << endl;
-    if (ec == std::errc::address_family_not_supported) cout << "we can compare error code against errc enumerators" << endl;
+    try { cout << std::stoi("NaN") << endl; }
+    catch (std::logic_error & e) { cout << "logic error: " << e.what() << endl; }
+    catch (std::runtime_error & e) { cout << "runtime error: " << e.what() << endl; }
+    catch (std::exception & e) { cout << "exception: " << e.what() << endl; }
 }
