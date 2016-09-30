93f2dbfbe9926f5626066f0b8f570a7153b6de63
heuristack <heuristack@gmail.com>
2016-09-30 19:18:48 +0800

    playground: function adaptor std::bind [bind particular arguments to and function object, return the new one]

--- main.cpp
+++ main.cpp
@@ -15,4 +15,10 @@ int main()
 {
     cout << Function()(100) << endl;
     cout << pass_function_object_to_me(Function(), 200) << endl;
+
+    Function F;
+    cout << pass_function_object_to_me(F, 400) << endl;
+
+    cout << std::bind(F, 500)() << endl;
+    cout << std::bind(F, std::placeholders::_1)(600) << endl;
 }
