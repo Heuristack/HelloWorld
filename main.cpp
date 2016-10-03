685a1a1fb33a6e410a8c80bc5146070a1efe16c8
heuristack <heuristack@gmail.com>
2016-10-03 11:03:15 +0800

    playground: default constructor, copy constructor, and std::function type

--- main.cpp
+++ main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <functional>
+#include <cstdlib>
 
 using std::cout;
+using std::ends;
 using std::endl;
 
 class Function {
 public:
-    Function(){}
+   ~Function(){ cout << "D" << ends; }
+    Function(){ cout << "F" << ends; }
+    Function(const Function&){ cout << "C" << ends; };
     int operator()(int i){ return i; }
 };
 
 int pass_function_object_to_me(Function f, int i){ return f(i); }
+
 int main()
 {
-    cout << Function()(100) << endl;
-    cout << pass_function_object_to_me(Function(), 200) << endl;
+    std::atexit([](){ cout << endl; });
+
+    cout << Function()(100);
+    cout << pass_function_object_to_me(Function(), 200);
 
     Function F;
-    cout << pass_function_object_to_me(F, 400) << endl;
+    cout << pass_function_object_to_me(F, 400);
 
-    cout << std::bind(F, 500)() << endl;
-    cout << std::bind(F, std::placeholders::_1)(600) << endl;
+    cout << std::bind(F, 500)();
+    cout << std::bind(F, std::placeholders::_1)(600);
+
+    std::function<int()> G = std::bind(F, 800);
+    cout << G();
 }
