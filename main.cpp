1d8ad8166d2bf46aaa51c965be69a914e084bdf3
heuristack <heuristack@gmail.com>
2016-10-04 16:53:34 +0800

    playground: static assert declaration could appear at namespace, block scope, and inside a class, etc

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
-#include <iostream>
 #include <type_traits>
-#include <string>
-
-using std::cout;
-using std::endl;
+template<typename T> struct S { static_assert(std::is_destructible<T>(), "Template argument should be destructible!"); };
 
 int main()
 {
-    cout << std::is_constructible<std::string>::value << endl;
-    cout << std::is_default_constructible<std::string>::value << endl;
-    cout << std::is_copy_constructible<std::string>::value << endl;
-    cout << std::is_move_constructible<std::string>::value << endl;
-
-    cout << std::is_assignable<std::string, std::string>::value << endl;
-    cout << std::is_copy_assignable<std::string>::value << endl;
-    cout << std::is_move_assignable<std::string>::value << endl;
-
-    cout << std::is_destructible<std::string>::value << endl;
+    class C { ~C() = delete; };
+    S<C> object;
 }
