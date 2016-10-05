a00e7b8c85a9c303c8b1c8d9fd2be6c2120c6d23
heuristack <heuristack@gmail.com>
2016-10-05 14:21:56 +0800

    playground: basic control constructs [if-else, switch, break, ect] (if 'break' is missed in one 'case')

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main()
 {
-    cout << __cplusplus << endl;
-    cout << __clang__ << endl;
-    cout << __VERSION__ << endl;
-
-    cout << __FILE__ << endl;
-    cout << __LINE__ << endl;
-    cout << __DATE__ << endl;
-    cout << __TIME__ << endl;
-
-    cout << __STDC__ << endl;
-    cout << __STDC_HOSTED__ << endl;
-
-    cout << __APPLE__ << endl;
-    cout << __MACH__ << endl;
+    bool test = true;
+    int casenum = 2;
+    if (test) {
+        switch(casenum) {
+            case 1:
+            case 2:
+                cout << "location 1" << endl;
+                // break;
+            default:
+                cout << "location 2" << endl;
+        }
+        cout << "location 3" << endl;
+    }
+    else {
+        cout << "location 4" << endl;
+    }
+    cout << "location 5" << endl;
 }
