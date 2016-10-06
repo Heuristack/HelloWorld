bb164976bdbf6793f69aa39d3ffefb0b526380f9
heuristack <heuristack@gmail.com>
2016-10-06 15:13:28 +0800

    playground: there is no enumeration of objects of any type but only integral in C++

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-#include <iostream>
+class T {
+    int value = 0;
+public:
+    T(int v):value(v){}
+    operator int(){ return value; }
+};
 
-enum level { warning = 2, notice = 3};
+enum E : T { zero = T(0), one = T(1), ten = T(10) };
 
-int main()
-{
-    level l;
-    l = warning;
-
-    switch (l) {
-        case warning:
-        case notice:
-            std::cout << l << std::endl;
-            break;
-    }
-}
+int main(){}
